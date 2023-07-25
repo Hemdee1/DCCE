96692381b9e8f619cc86746af7340b0cd2941179
hemdee <sanusi5110@gmail.com>
2023-07-25 18:53:53 +0100

    update meta data

--- pages/_document.js
+++ pages/_document.js
@@ -4,16 +4,25 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta property="title" content="ATBU DCCE Handbook" />
         <meta
           name="description"
           content="A student guide that makes learning more accessible to Department of Computer and Communication Engineering Students"
           key="desc"
         />
+        <meta
+          name="keywords"
+          content="ATBU, DCCE, Computer Engineering, Communication Engineering, Abubakar Tafawa Balewa University, Student Handbook, ATBU Student Handbook, DCCE Student Handbook, courses"
+        ></meta>
         <meta property="og:title" content="ATBU DCCE Handbook" />
         <meta
           property="og:description"
           content="A student guide that makes learning more accessible to Department of Computer and Communication Engineering Students"
         />
+        <meta
+          name="og:keywords"
+          content="ATBU, DCCE, Computer Engineering, Communication Engineering, Abubakar Tafawa Balewa University, Student Handbook, ATBU Student Handbook, DCCE Student Handbook, courses"
+        ></meta>
         <meta property="og:image" content="/public/images/dept.jpeg" />
       </Head>
       <body>
